a409a7f6cc6ac6d356a5551d6fb20ff92bbbc5ae
mikodavinz <vincenzo.micciche.27@gmail.com>
2025-03-29 17:07:31 +0100

    -test

--- src/gameobject.js
+++ src/gameobject.js
@@ -1,12 +1,91 @@
 import * as THREE from 'three';
+import * as CANNON from 'cannon-es';
 class GameObject{
 
-    constructor(name, origin){
-        this.name=name;
-        this.origin=origin;
+    constructor(obj_name, geometry, material, scene, world,
+                position = new THREE.Vector3(0, 0, 0),  
+                rotation = new THREE.Quaternion(), 
+                scale = new THREE.Vector3(1, 1, 1),
+                physicsBodyProperties = {}, physicsShape=null) {
+        
+        this.obj_name = obj_name;
+        this.geometry = geometry;
+        this.material = material;
+        this.scene = scene;
+        this.world = world;
+        this.position = position;
+        this.rotation = rotation; 
+        this.scale = scale;
+
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.mesh.position.x = this.position.x;
+        this.mesh.position.y = this.position.y;
+        this.mesh.position.z = this.position.z;
+        this.mesh.quaternion.x = this.rotation.x;
+        this.mesh.quaternion.y = this.rotation.y;
+        this.mesh.quaternion.z = this.rotation.z;
+        this.mesh.quaternion.w = this.rotation.w;
+        this.mesh.scale.set(this.scale.x, this.scale.y, this.scale.z);
+
+        this.mesh.castShadow = true;
+        this.mesh.receiveShadow = true;
+        
+
+        this.createPhysicsBody(physicsBodyProperties);
+        if(!physicsShape){
+            this.body.addShape(new CANNON.Box(new CANNON.Vec3(this.geometry.parameters.width/2, this.geometry.parameters.height/2, this.geometry.parameters.depth/2)));
+            console.log("CREATED BOX SHAPE FOR OBJECT "+this.name);
+        }else{
+            this.body.addShape(physicsShape);
+
+        }
+        
     }
 
-    add_to_scene(scene){
-        scene.add(this)
+    createPhysicsBody(properties={}){
+        
+        var def_properties = {
+            mass:1,
+            // angularDamping:0.5,
+            // fixedRotation:true
+        }
+
+        const merged={... def_properties, ...properties}
+
+        this.body=new CANNON.Body(merged);
+
+        this.body.position.x=this.position.x;
+        this.body.position.y=this.position.y;   
+        this.body.position.z=this.position.z;
+
+        this.body.quaternion.x=this.rotation.x;
+        this.body.quaternion.y=this.rotation.y;
+        this.body.quaternion.z=this.rotation.z;
+        this.body.quaternion.w=this.rotation.w;
+        this.body.name=this.obj_name;
+
+        this.body.addEventListener("collide", function(event){
+            console.log("Collision detected between "+event.body.name+" and "+event.target.name);
+            // console.log(event);
+        });
+        
+    }
+
+    instantiate(){
+        this.scene.add(this.mesh);
+        this.world.addBody(this.body);
+    }
+
+    update(){
+        this.position.set(this.body.position.x, this.body.position.y, this.body.position.z);
+        this.rotation.set(this.body.quaternion.x, this.body.quaternion.y, this.body.quaternion.z, this.body.quaternion.w);
+
+        this.mesh.position.set(this.body.position.x, this.body.position.y, this.body.position.z);
+        this.mesh.quaternion.set(this.body.quaternion.x, this.body.quaternion.y, this.body.quaternion.z, this.body.quaternion.w);
+        
+        
+        // console.log("Updating GameObject "+this.obj_name+" with position "+this.position.toArray()+" and rotation "+this.rotation.toArray());
     }
 }
+
+export {GameObject};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import * as THREE from '../three';
+import * as THREE from 'three';
+import * as CANNON from 'cannon-es';
+import * as GAMEOBJ from './gameobject.js';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 
 let scene, camera, renderer, controls;
@@ -6,77 +8,109 @@ let moveForward = false, moveBackward = false, moveLeft = false, moveRight = fal
 let velocity = new THREE.Vector3();
 let direction = new THREE.Vector3();
 const speed = 5;
-
+const clock = new THREE.Clock();
+let delta=0.1
 // Initialize Scene
 
-function init() {
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x87CEEB); // Sky color
+scene = new THREE.Scene();
+scene.background = new THREE.Color(0x87CEEB); // Sky color
 
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    cube.position.y=4;
-    cube.receiveShadow=true;
-    cube.castShadow=true;
-    scene.add( cube );
+const world= new CANNON.World();
+world.gravity.set(0, -9.82, 0); // Set gravity
+world.broadphase = new CANNON.NaiveBroadphase()
+world.solver.iterations = 10
+world.allowSleep = true
 
-    const dirLight = new THREE.DirectionalLight( 0xffffff, 3 );
-    dirLight.position.set( -5, 10, 0 );
-    dirLight.castShadow = true;
-    dirLight.shadow.camera.top = 2;
-    dirLight.shadow.camera.bottom = - 2;
-    dirLight.shadow.camera.left = - 2;
-    dirLight.shadow.camera.right = 2;
-    dirLight.shadow.camera.near = 0.1;
-    dirLight.shadow.camera.far = 100;
-    dirLight.target.position.set(0,0,0)
-    scene.add( dirLight );
-    
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.y = 5;
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.shadowMap.enabled = true;
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+const cube= new GAMEOBJ.GameObject("cube", geometry, material, scene, world, 
+    new THREE.Vector3(4, 3, -5), 
+    new THREE.Quaternion(), 
+    new THREE.Vector3(1, 1, 1), 
+    {mass:1},
+    new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)));
+cube.instantiate();
 
-    // Pointer Lock Controls
-    controls = new PointerLockControls(camera, document.body);
-    controls.pointerSpeed=5
-    document.getElementById("instructions").addEventListener('click', () => {
-        controls.lock();
-    });
+const objects = [];
 
-    controls.addEventListener('lock', () => {
-        document.getElementById("instructions").style.display = 'none';
-    });
+const dirLight = new THREE.DirectionalLight( 0xffffff, 3 );
+dirLight.position.set( 0, 10, 0 );
+dirLight.castShadow = true;
+dirLight.shadow.camera.top = 10;
+dirLight.shadow.camera.bottom = - 10;
+dirLight.shadow.camera.left = - 10;
+dirLight.shadow.camera.right = 10;
+dirLight.shadow.camera.near = 0.1;
+dirLight.shadow.camera.far = 200;
+dirLight.shadow.normalBias=0.001;
+dirLight.target.position.set(0,0,0)
+scene.add( dirLight );
 
-    controls.addEventListener('unlock', () => {
-        document.getElementById("instructions").style.display = 'flex';
-    });
+camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = 5;
 
-    scene.add(controls.getObject());
+renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.domElement.id='canvas'
+document.body.appendChild(renderer.domElement);
 
-    // Ground
-    const groundGeometry = new THREE.PlaneGeometry(200, 200);
-    const groundMaterial = new THREE.MeshPhongMaterial({ color: 0x228B22, side: THREE.DoubleSide });
-    const ground = new THREE.Mesh(groundGeometry, groundMaterial);
-    ground.receiveShadow=true;
-    ground.rotation.x = -Math.PI / 2;
-    scene.add(ground);
+// Pointer Lock Controls
+controls = new PointerLockControls(camera, document.body);
+controls.pointerSpeed=5
+document.getElementById("instructions").addEventListener('click', () => {
+    controls.lock();
+});
 
-    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x8d8d8d, 3 );
-    hemiLight.position.set( 0, 20, 0 );
-    scene.add( hemiLight );
-    // Event Listeners for Movement
-    document.addEventListener('keydown', onKeyDown);
-    document.addEventListener('keyup', onKeyUp);
+controls.addEventListener('lock', () => {
+    document.getElementById("instructions").style.display = 'none';
+});
 
-    window.addEventListener('resize', onWindowResize);
-    
-    animate();
-}
+controls.addEventListener('unlock', () => {
+    document.getElementById("instructions").style.display = 'flex';
+});
+
+scene.add(controls.getObject());
+
+// Ground
+const groundGeometry = new THREE.PlaneGeometry(200, 200);
+const groundMaterial = new THREE.MeshPhongMaterial({ color: 0x228B22, side: THREE.DoubleSide });
+const ground = new GAMEOBJ.GameObject("ground", groundGeometry, groundMaterial, scene, world, 
+                            new THREE.Vector3(0, 0, 0), 
+                            new THREE.Quaternion().setFromEuler(new THREE.Euler( - Math.PI / 2, 0, 0) ),
+                            new THREE.Vector3(1, 1, 1), 
+                            {mass:0},
+                        new CANNON.Plane());
+ground.instantiate();
+const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x8d8d8d, 3 );
+hemiLight.position.set( 0, 20, 0 );
+scene.add( hemiLight );
+// Event Listeners for Movement
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+
+window.addEventListener('resize', onWindowResize);
+
+const normalMaterial = new THREE.MeshNormalMaterial()
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMesh = new THREE.Mesh(cubeGeometry, material)
+cubeMesh.position.x = -3
+cubeMesh.position.z = -3
+cubeMesh.position.y = 3
+cubeMesh.castShadow = true
+cubeMesh.receiveShadow = true
+// scene.add(cubeMesh)s
+const cubeShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5))
+const cubeBody = new CANNON.Body({ mass: 0  })
+cubeBody.addShape(cubeShape)
+cubeBody.position.x = cubeMesh.position.x
+cubeBody.position.y = cubeMesh.position.y
+cubeBody.position.z = cubeMesh.position.z
+// world.addBody(cubeBody)s
+
+
 
 function onKeyDown(event) {
     console.log(event.code)
@@ -101,9 +135,13 @@ function onKeyUp(event) {
     }
 }
 
+
+
 function animate() {
     requestAnimationFrame(animate);
 
+    delta = Math.min(clock.getDelta(), 0.1)
+    world.step(delta)
     if (controls.isLocked) {
         direction.z = Number(moveForward) - Number(moveBackward);
         direction.x = Number(moveRight) - Number(moveLeft);
@@ -117,9 +155,19 @@ function animate() {
         controls.moveRight(-velocity.x * 0.1);
         controls.moveForward(-velocity.z * 0.1);
 
+        camera.position.y += velocity.y * 0.1;
+
+        velocity.y *= 0.9;
         velocity.x *= 0.9;
         velocity.z *= 0.9;
     }
+
+    cube.update();
+    ground.update();
+
+    cubeMesh.position.set(cubeBody.position.x, cubeBody.position.y, cubeBody.position.z)
+    cubeMesh.quaternion.set(cubeBody.quaternion.x, cubeBody.quaternion.y, cubeBody.quaternion.z, cubeBody.quaternion.w)
+  
 
     renderer.render(scene, camera);
 }
@@ -128,6 +176,8 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+
+    
 }
 
-init();
+animate();
